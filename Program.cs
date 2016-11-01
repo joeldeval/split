f0c49ef5e9dcfc2d70b104efed2f9230782815d3
Joel Valdivia <jooelvaldivi@gmail.com>
2016-11-01 17:12:42 -0600

    create program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SplitEjemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            string texto = "Lorem ipsum dolor sit amet\r\nconsectetur adipiscing elit.\r\nCurabitur ac lacinia massa.\r\n Etiam lorem felis, dignissim nec tristique non, sodales quis tellus.";
+            string[] arregloTexto = texto.Split('\r', '\n').Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            
+            foreach(string dato in arregloTexto){
+                Console.Write(dato);
+            }
+            
+
+        }
+
+    }
+}
